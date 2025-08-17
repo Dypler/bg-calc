87dac36fcea1048f646a7632b2d84e8a48bc3515
Saveliy Suntsov <sun.zakup@mail.ru>
2025-08-17 15:33:46 +0400

    Корректировка конфига

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
-        execution: resolve(__dirname, 'execution.html'),
-        warranty: resolve(__dirname, 'warranty.html'),
-        bid: resolve(__dirname, 'bid.html'),
-        advance: resolve(__dirname, 'advance.html'),
-        payment: resolve(__dirname, 'payment.html')
       }
     }
   }
